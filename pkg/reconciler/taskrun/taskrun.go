390fe90b17c4a379b580d1d05ed9e2f3576f3fa5
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-08-10 09:54:52 +1000

    More work

--- pkg/reconciler/taskrun/taskrun.go
+++ pkg/reconciler/taskrun/taskrun.go
@@ -2,6 +2,7 @@ package taskrun
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	v12 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -12,6 +13,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -122,7 +125,7 @@ func (r *ReconcileTaskRun) handleUserTask(ctx context.Context, log logr.Logger, 
 	secretName := "multi-arch-ssl-" + tr.Name
 	err := r.client.Get(ctx, types.NamespacedName{Namespace: tr.Namespace, Name: secretName}, &secret)
 	if err == nil {
-		return r.handleExistingSecret(ctx, log, tr, secret)
+		return r.handleExistingSecret(ctx, log, tr, &secret)
 	} else if errors.IsNotFound(err) {
 		//if the PR is done we ignore it
 		if tr.Status.CompletionTime != nil || tr.GetDeletionTimestamp() != nil {
@@ -136,26 +139,63 @@ func (r *ReconcileTaskRun) handleUserTask(ctx context.Context, log logr.Logger, 
 }
 
 func (r *ReconcileTaskRun) handleHostAllocation(ctx context.Context, tr *v1beta1.TaskRun, secret v12.Secret, secretName string) (reconcile.Result, error) {
-	//lets allocate a host
+	targetArch := tr.Labels[TargetArchitectureLabel]
 
-	cm := v12.ConfigMap{}
-	err := r.client.Get(ctx, types.NamespacedName{Namespace: r.operatorNamespace, Name: HostConfig}, &cm)
+	//lets allocate a host, get the map with host info
+	hosts, err := r.hostConfig(ctx)
 	if err != nil {
 		return reconcile.Result{}, err
 	}
+	//get all existing runs that are assigned to a host
+	taskList := v1beta1.TaskRunList{}
+	err = r.client.List(ctx, &taskList, client.HasLabels{AssignedHost}, client.MatchingLabels{TargetArchitectureLabel: targetArch})
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	hostCount := map[string]int{}
+	for _, tr := range taskList.Items {
+		host := tr.Labels[AssignedHost]
+		hostCount[host] = hostCount[host] + 1
+	}
 
-	//just copy our hard coded secret
-	arch := tr.Labels[TargetArchitectureLabel]
+	//now select the host with the most free spots
+	//this algorithm is not very complex
+
+	var selected *Host
+	freeSpots := 0
+	for k, v := range hosts {
+		if v.Arch != targetArch {
+			continue
+		}
+		free := v.Concurrency - hostCount[k]
+		if free > freeSpots {
+			selected = v
+			freeSpots = free
+		}
+	}
+	if selected == nil {
+		//no host available
+		//add the waiting label
+		//TODO: is the requeue actually a good idea?
+		tr.Labels[WaitingForArchLabel] = targetArch
+		return reconcile.Result{RequeueAfter: time.Minute}, r.client.Update(ctx, tr)
+	}
+	tr.Labels[AssignedHost] = selected.Name
+
+	//create the host secret
+	arch := targetArch
 	secret.Labels = map[string]string{TargetArchitectureLabel: arch}
 	secret.Namespace = tr.Namespace
 	secret.Name = secretName
 
 	hardCoded := v12.Secret{}
-	err = r.client.Get(ctx, types.NamespacedName{Namespace: "multi-arch-operator", Name: "tmp"}, &hardCoded)
+	err = r.client.Get(ctx, types.NamespacedName{Namespace: r.operatorNamespace, Name: selected.Secret}, &hardCoded)
 	if err != nil {
 		return reconcile.Result{}, err
 	}
-	secret.Data = hardCoded.Data
+	secret.Data["id_rsa"] = secret.Data["id_rsa"]
+	secret.Data["host"] = []byte(selected.User + "@" + selected.Address)
+	secret.Data["build-dir"] = []byte("/tmp/" + uuid.New().String())
 	err = r.client.Create(ctx, &secret)
 	if err != nil {
 		return reconcile.Result{}, err
@@ -165,11 +205,11 @@ func (r *ReconcileTaskRun) handleHostAllocation(ctx context.Context, tr *v1beta1
 	return reconcile.Result{}, r.client.Update(ctx, tr)
 }
 
-func (r *ReconcileTaskRun) handleExistingSecret(ctx context.Context, log logr.Logger, tr *v1beta1.TaskRun, secret v12.Secret) (reconcile.Result, error) {
+func (r *ReconcileTaskRun) handleExistingSecret(ctx context.Context, log logr.Logger, tr *v1beta1.TaskRun, secret *v12.Secret) (reconcile.Result, error) {
 	//already exists
 	if tr.Status.CompletionTime != nil || tr.GetDeletionTimestamp() != nil {
 		//PR is done, clean up the secret
-		err := r.client.Delete(ctx, &secret)
+		err := r.client.Delete(ctx, secret)
 		if err != nil {
 			log.Error(err, "Unable to delete secret")
 		}
@@ -182,4 +222,51 @@ func (r *ReconcileTaskRun) handleExistingSecret(ctx context.Context, log logr.Lo
 		return r.handleWaitingTasks(tr.Labels[TargetArchitectureLabel])
 	}
 	return reconcile.Result{}, nil
+}
+
+func (r *ReconcileTaskRun) hostConfig(ctx context.Context) (map[string]*Host, error) {
+	cm := v12.ConfigMap{}
+	err := r.client.Get(ctx, types.NamespacedName{Namespace: r.operatorNamespace, Name: HostConfig}, &cm)
+	if err != nil {
+		return nil, err
+	}
+	ret := map[string]*Host{}
+	for k, v := range cm.Data {
+		pos := strings.LastIndex(k, ".")
+		name := k[0:pos]
+		key := k[pos+1:]
+		host := ret[key]
+		if host == nil {
+			host = &Host{}
+			ret[name] = host
+			host.Name = name
+		}
+		switch name {
+		case "address":
+			host.Address = v
+		case "user":
+			host.User = v
+		case "arch":
+			host.Arch = v
+		case "secret":
+			host.Secret = v
+		case "concurrency":
+			atoi, err := strconv.Atoi(v)
+			if err != nil {
+				return nil, err
+			}
+			host.Concurrency = atoi
+		}
+
+	}
+	return ret, nil
+}
+
+type Host struct {
+	Address     string
+	Name        string
+	User        string
+	Concurrency int
+	Arch        string
+	Secret      string
 }
