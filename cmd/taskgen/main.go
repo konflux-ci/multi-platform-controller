52226d1d01259f1476c67cbd7f8937232a684869
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-09-04 18:23:37 +1000

    Minor image fix

--- cmd/taskgen/main.go
+++ cmd/taskgen/main.go
@@ -194,7 +194,7 @@ fi
 
 func replaceImage(image string) string {
 	if image == "quay.io/redhat-appstudio/buildah:v1.28" {
-		return "quay.io/buildah/stable:v1.28"
+		return "quay.io/redhat-appstudio/buildah:v1.31"
 	}
 	return image
 }
