7e031e4ea0626ce4ad789139e2b72c0a931e0223
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-10-23 14:02:55 +1100

    STONEBLD-1868 Initial OTP server work

---
+++ cmd/otp/otp.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"crypto/rand"
+	"github.com/go-logr/logr"
+	"io"
+	"math/big"
+	"net/http"
+	"sync"
+)
+
+var mutex = sync.Mutex{}
+var globalMap = map[string][]byte{}
+
+// otp service example implementation.
+// The example methods log the requests and return zero values.
+type storekey struct {
+	logger *logr.Logger
+}
+
+func (s *storekey) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		s.logger.Error(err, "failed to read request body", "address", request.RemoteAddr)
+		writer.WriteHeader(500)
+		return
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+	otp, err := GenerateRandomString(20)
+	if err != nil {
+		s.logger.Error(err, "failed to generate OTP password", "address", request.RemoteAddr)
+		writer.WriteHeader(500)
+		return
+	}
+	globalMap[otp] = body
+	_, err = writer.Write([]byte(otp))
+	if err != nil {
+		s.logger.Error(err, "failed to write http response", "address", request.RemoteAddr)
+		writer.WriteHeader(500)
+	} else {
+		s.logger.Info("stored SSH key in OTP map", "address", request.RemoteAddr)
+	}
+}
+
+// NewOtp returns the otp service implementation.
+func NewStoreKey(logger *logr.Logger) *storekey {
+	return &storekey{logger}
+}
+
+type otp struct {
+	logger *logr.Logger
+}
+
+// NewOtp returns the otp service implementation.
+func NewOtp(logger *logr.Logger) *otp {
+	return &otp{logger}
+}
+
+func (s *otp) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		s.logger.Error(err, "failed to read request body")
+		writer.WriteHeader(500)
+		return
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	res, loaded := globalMap[string(body)]
+	delete(globalMap, string(body))
+	if !loaded {
+		writer.WriteHeader(400)
+	} else {
+		_, err := writer.Write(res)
+		if err != nil {
+			s.logger.Error(err, "failed to write http response", "address", request.RemoteAddr)
+			writer.WriteHeader(500)
+		} else {
+			s.logger.Info("served one time password", "address", request.RemoteAddr)
+		}
+	}
+}
+
+// GenerateRandomString returns a securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret), nil
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.18.37
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.114.0
 	github.com/go-logr/logr v1.2.4
-	github.com/google/uuid v1.3.0
+	github.com/google/uuid v1.3.1
 	github.com/onsi/gomega v1.27.10
 	github.com/pkg/errors v0.9.1
 	github.com/tektoncd/pipeline v0.47.3
@@ -63,7 +63,7 @@ require (
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.1 // indirect
 	github.com/go-openapi/strfmt v0.21.5 // indirect
-	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.13.0 // indirect
@@ -102,23 +102,25 @@ require (
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/exp v0.0.0-20230307190834-24139beb5833 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/oauth2 v0.7.0 // indirect
-	golang.org/x/sync v0.2.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/term v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.3.0 // indirect
 	google.golang.org/api v0.116.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230331144136-dcfb400f0633 // indirect
-	google.golang.org/grpc v1.54.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/genproto v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+	google.golang.org/grpc v1.58.2 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
