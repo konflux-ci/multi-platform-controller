de1ab336910d404a29ac4c681d8deb8981514302
mrath <mrath@redhat.com>
2024-10-29 23:02:49 +0200

    Add HostUpdateTaskRunTest spec

--- pkg/reconciler/taskrun/hostupdate_test.go
+++ pkg/reconciler/taskrun/hostupdate_test.go
@@ -2,7 +2,9 @@ package taskrun
 
 import (
 	"context"
+
 	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -11,65 +13,159 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
-	"testing"
-	"time"
 )
 
 const testNamespace = "default"
 
-var scheme = runtime.NewScheme()
+func hostDataFromTRSpec(specParams v1.Params) map[string]string {
+	newHostData := make(map[string]string)
 
-func init() {
-	utilruntime.Must(corev1.AddToScheme(scheme))
-	utilruntime.Must(v1.AddToScheme(scheme))
-}
-
-func TestTaskRunCreates(t *testing.T) {
-	g := NewGomegaWithT(t)
-
-	preexisting := &corev1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      HostConfig,
-			Namespace: testNamespace,
-		},
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "ConfigMap",
-			APIVersion: "v1",
-		},
-		Data: map[string]string{
-			"host.power-rhtap-prod-1.address":  "10.130.75.23",
-			"host.power-rhtap-prod-1.platform": "linux/ppc64le",
-			// TODO: add the rest values
-			//host.power-rhtap-prod-1.user: "root"
-			//host.power-rhtap-prod-1.secret: "internal-prod-ibm-ssh-key"
-			//host.power-rhtap-prod-1.concurrency: "1"
-
-		},
+	for _, specParam := range specParams {
+		switch key := specParam.Name; key {
+		case "address":
+			newHostData["address"] = specParam.Value.StringVal
+		case "secret":
+			newHostData["secret"] = specParam.Value.StringVal
+		case "concurrency":
+			newHostData["concurrency"] = specParam.Value.StringVal
+		case "user":
+			newHostData["user"] = specParam.Value.StringVal
+		case "platform":
+			newHostData["platform"] = specParam.Value.StringVal
+		default:
+			// Not really needed
+		}
 	}
 
-	k8sClient := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(preexisting).Build()
-	log := logr.FromContextOrDiscard(context.TODO())
-
-	// tested function call
-	UpdateHostPools(testNamespace, k8sClient, &log)
-
-	// get list of all TaskRuns, as we cannot predict the name
-	createdList := v1.TaskRunList{}
-
-	time.Sleep(3 * time.Second) //TODO: try to replace with Eventually() as by example below
-
-	//Eventually(func(g Gomega) {
-	//	g.Expect(k8sClient.List(context.TODO(), &createdList, client.InNamespace(testNamespace))).To(Succeed())
-	//	g.Expect(len(createdList.Items)).To(Equal(1))
-	//}).Should(Succeed())
-
-	g.Expect(k8sClient.List(context.TODO(), &createdList, client.InNamespace(testNamespace))).To(Succeed())
-	g.Expect(len(createdList.Items)).To(Equal(1))
-
-	createdTR := createdList.Items[0]
-
-	g.Expect(createdTR.Labels).To(HaveKeyWithValue(TaskTypeLabel, TaskTypeUpdate))
-	//TODO: verify the rest of the important resulting TaskRun fields, ie. rest of labels, spec.params, workspaces etc
+	return newHostData
 }
 
-//TODO: try to find other scenarios to test
+var _ = Describe("HostUpdateTaskRunTest", func() {
+	var scheme = runtime.NewScheme()
+	var hostConfig = &corev1.ConfigMap{}
+
+	BeforeEach(func() {
+		utilruntime.Must(corev1.AddToScheme(scheme))
+		utilruntime.Must(v1.AddToScheme(scheme))
+
+		hostConfig = &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      HostConfig,
+				Namespace: testNamespace,
+			},
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ConfigMap",
+				APIVersion: "v1",
+			},
+			Data: map[string]string{"test data": "will replace this"},
+		}
+	})
+
+	DescribeTable("Creating taskruns for updating static hosts in a pool",
+		func(hostConfigData map[string]string, hostUpdateTaskRunCreated bool,
+			oneTaskRunCreated bool, labelFieldsCorrectlyFilled bool,
+			addressParamCorrect bool,
+			secretParamCorrect bool,
+			concurrencyParamCorrect bool,
+			userParamCorrect bool,
+			platformParamCorrect bool) {
+
+			hostConfig.Data = hostConfigData
+
+			k8sClient := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(hostConfig).Build()
+			log := logr.FromContextOrDiscard(context.TODO())
+
+			// tested function call
+			UpdateHostPools(testNamespace, k8sClient, &log)
+
+			// get list of all TaskRuns, as we cannot predict the name
+			createdList := v1.TaskRunList{}
+
+			// test everything in TaskRun creation that is not part of the table testing
+			Eventually(func(g Gomega) {
+				// TaskRun successfully created
+				g.Expect(k8sClient.List(context.TODO(), &createdList, client.InNamespace(testNamespace))).To(Succeed())
+				// Only one TaskRun was created
+				g.Expect(len(createdList.Items)).To(Equal(1))
+				// set label field filled correctly
+				Expect(createdList.Items[0].Labels).To(HaveKeyWithValue(TaskTypeLabel, TaskTypeUpdate))
+			}).Should(Succeed())
+
+			// extract TaskRun data to begin testing individual fields were correctly filled
+			updatedHostData := hostDataFromTRSpec(createdList.Items[0].Spec.Params)
+
+			// validate each field is exactly as it's expected to be
+			Expect(updatedHostData["address"]).To(Equal(hostConfigData["address"]))
+			Expect(updatedHostData["secret"]).To(Equal(hostConfigData["secret"]))
+			Expect(updatedHostData["concurrency"]).To(Equal(hostConfigData["concurrency"]))
+			Expect(updatedHostData["user"]).To(Equal(hostConfigData["user"]))
+			Expect(updatedHostData["platform"]).To(Equal(hostConfigData["platform"]))
+		},
+		Entry("Positive test", map[string]string{
+			"host.koko-hazamar-prod-1.address":     "10.130.75.23",
+			"host.koko-hazamar-prod-1.secret":      "internal-prod-ibm-ssh-key",
+			"host.koko-hazamar-prod-1.concurrency": "1",
+			"host.koko-hazamar-prod-1.user":        "koko_hazamar",
+			"host.koko-hazamar-prod-1.platform":    "linux/ppc64le"},
+			true, true, true, true, true, true, true, true),
+		// Entry("Positive test - ", map[string]string{}),
+		Entry("Negative test - empty data map", map[string]string{},
+			true, true, true, false, false, false, false, false),
+		Entry("Negative test - dynamic host keys", map[string]string{
+			"dynamic.moshe-kipod-prod-1.address":     "10.130.75.23",
+			"dynamic.moshe-kipod-prod-1.secret":      "internal-prod-ibm-ssh-key",
+			"dynamic.moshe-kipod-prod-1.concurrency": "1",
+			"dynamic.moshe-kipod-prod-1.user":        "koko_hazamar",
+			"dynamic.moshe-kipod-prod-1.platform":    "linux/ppc64le"},
+			false, false, false, false, false, false, false, false),
+		Entry("Negative test - bad key format", map[string]string{
+			"host.address":     "10.130.75.23",
+			"host.secret":      "internal-prod-ibm-ssh-key",
+			"host.concurrency": "1",
+			"host.user":        "root",
+			"host.platform":    "linux/ppc64le"},
+			false, false, false, false, false, false, false, false),
+		Entry("Negative test - bad address field", map[string]string{
+			"host.koko-hazamar-prod-1.address":     "10.130",
+			"host.koko-hazamar-prod-1.secret":      "internal-prod-ibm-ssh-key",
+			"host.koko-hazamar-prod-1.concurrency": "1",
+			"host.koko-hazamar-prod-1.user":        "koko_hazamar",
+			"host.koko-hazamar-prod-1.platform":    "linux/ppc64le"},
+			false, false, false, false, false, false, false, false),
+		Entry("Negative test - bad secret field", map[string]string{
+			"host.koko-hazamar-prod-1.address":     "10.130.75.23",
+			"host.koko-hazamar-prod-1.secret":      "",
+			"host.koko-hazamar-prod-1.concurrency": "1",
+			"host.koko-hazamar-prod-1.user":        "koko_hazamar",
+			"host.koko-hazamar-prod-1.platform":    "linux/ppc64le"},
+			false, false, false, false, false, false, false, false),
+		Entry("Negative test - bad concurrency part I", map[string]string{
+			"host.koko-hazamar-prod-1.address":     "10.130.75.23",
+			"host.koko-hazamar-prod-1.secret":      "internal-prod-ibm-ssh-key",
+			"host.koko-hazamar-prod-1.concurrency": "-1",
+			"host.user":                            "koko_hazamar",
+			"host.koko-hazamar-prod-1.platform":    "linux/ppc64le"},
+			false, false, false, false, false, false, false, false),
+		Entry("Negative test - bad concurrency part II", map[string]string{
+			"host.koko-hazamar-prod-1.address":     "10.130.75.23",
+			"host.koko-hazamar-prod-1.secret":      "internal-prod-ibm-ssh-key",
+			"host.koko-hazamar-prod-1.concurrency": "1234567890",
+			"host.user":                            "koko_hazamar",
+			"host.koko-hazamar-prod-1.platform":    "linux/ppc64le"},
+			false, false, false, false, false, false, false, false),
+		Entry("Negative test - bad user", map[string]string{
+			"host.koko-hazamar-prod-1.address":     "10.130.75.23",
+			"host.koko-hazamar-prod-1.secret":      "internal-prod-ibm-ssh-key",
+			"host.koko-hazamar-prod-1.concurrency": "1",
+			"host.koko-hazamar-prod-1.user":        "root",
+			"host.koko-hazamar-prod-1.platform":    "linux/ppc64le"},
+			false, false, false, false, false, false, false, false),
+		Entry("Negative test - bad platform", map[string]string{
+			"host.koko-hazamar-prod-1.address":     "10.130.75.23",
+			"host.koko-hazamar-prod-1.secret":      "internal-prod-ibm-ssh-key",
+			"host.koko-hazamar-prod-1.concurrency": "1",
+			"host.koko-hazamar-prod-1.user":        "koko_hazamar",
+			"host.koko-hazamar-prod-1.platform":    "linux/moshe_kipod555"},
+			false, false, false, false, false, false, false, false),
+	)
+})
