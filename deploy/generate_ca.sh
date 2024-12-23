8da4c3f23f4d1251b620c0d85cc6d91753f5cdaf
Max Shaposhnyk <mshaposh@redhat.com>
2024-12-23 09:52:50 +0200

    Add local development scripts (#361)
    * Add local development scripts
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>
    
    * Fixups
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>
    
    ---------
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>

---
+++ deploy/generate_ca.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  This script generates the certificates needed for the otp
+
+set -e
+
+
+THIS_DIR="$(dirname "$(realpath "$0")")"
+TEMP_DIR="${THIS_DIR}/../.tmp"
+
+GENCERTS_DIR="${GENCERTS_DIR:-"${TEMP_DIR}/k8s/certs"}"
+
+echo "Generating CA bundle"
+mkdir -p "${GENCERTS_DIR}"
+
+openssl genrsa -out ${GENCERTS_DIR}/ca.key 2048
+openssl req -x509 -new -nodes -key ${GENCERTS_DIR}/ca.key -subj "/CN=otp-service.mpc.svc" -days 365 -out ${GENCERTS_DIR}/ca.crt
+
+# verify created
+cat ${GENCERTS_DIR}/ca.crt >/dev/null
+if [ $? -eq 0 ]; then
+    echo "CA bundle created"
+  else
+    echo "CA generation failed"
+    exit 1
+fi
