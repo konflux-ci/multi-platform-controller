892bd1550be7a4e11e5424c129c863d3cb1ffe60
mrath <mrath@redhat.com>
2025-01-16 23:49:54 +0200

    Complete test for ibmp.go - testing IBMPowerProvider function

--- pkg/ibm/ibmp_test.go
+++ pkg/ibm/ibmp_test.go
@@ -1,71 +1,69 @@
 package ibm
 
 import (
-	"context"
+	"encoding/base64"
+	"strconv"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
-	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-var config = IBMPowerDynamicConfig{
-	SystemNamespace: "test-namespace",
-	Secret:          "test-secret",
-	Key:             "test-key",
-	Image:           "test-image",
-	Url:             "test-url",
-	CRN:             "test-crn",
-	Network:         "test-network",
-	Cores:           2,
-	Memory:          4,
-	Disk:            10,
-	System:          "test-system",
-	UserData:        "test-user-data",
-	ProcType:        "test-proc-type",
+const systemNamespace = "multi-platform-controller"
+
+func parseFloat(s string) float64 {
+	f, err := strconv.ParseFloat(s, 64)
+	Expect(err).NotTo(HaveOccurred())
+	return f
 }
 
-func setupClientAndConfig(config IBMPowerDynamicConfig) (client.Client, *IBMPowerDynamicConfig) {
-	scheme := runtime.NewScheme()
-	_ = pipelinev1.AddToScheme(scheme)
-	_ = v1.AddToScheme(scheme)
-	_ = appsv1.AddToScheme(scheme)
-	objs := []client.Object{}
-	objs = append(objs, &v1.Namespace{
-		ObjectMeta: v1.ObjectMeta{
-			Name: config.SystemNamespace,
-		},
-	})
-	client := fake.NewClientBuilder().WithScheme(scheme).WithObjects(objs...).Build()
-	return client, &config
-}
+var _ = DescribeTable("IBMPowerProvider unit test",
+	func(platform string, testConfig map[string]string, expectedCores string, expectedMemory string, expectedDisk string) {
+		config := map[string]string{
+			"dynamic." + platform + ".key":       "test-key",
+			"dynamic." + platform + ".image":     "test-image",
+			"dynamic." + platform + ".secret":    "test-secret",
+			"dynamic." + platform + ".url":       "test-url",
+			"dynamic." + platform + ".crn":       "test-crn",
+			"dynamic." + platform + ".network":   "test-network",
+			"dynamic." + platform + ".system":    "test-system",
+			"dynamic." + platform + ".user-data": "test-userData",
+			"dynamic." + platform + ".memory":    testConfig["memory"],
+			"dynamic." + platform + ".cores":     testConfig["cores"],
+			"dynamic." + platform + ".disk":      testConfig["disk"]}
 
-var _ = Describe("IBM PowerPC Tests", func() {
+		provider := IBMPowerProvider(platform, config, systemNamespace)
+		Expect(provider).ToNot(BeNil())
+		providerConfig := provider.(IBMPowerDynamicConfig)
+		Expect(providerConfig).ToNot(BeNil())
 
-	client, _ := setupClientAndConfig(config)
+		Expect(providerConfig.Key).To(Equal("test-key"))
+		Expect(providerConfig.Image).To(Equal("test-image"))
+		Expect(providerConfig.Secret).To(Equal("test-secret"))
+		Expect(providerConfig.Url).To(Equal("test-url"))
+		Expect(providerConfig.CRN).To(Equal("test-crn"))
+		Expect(providerConfig.Network).To(Equal("test-network"))
+		Expect(providerConfig.System).To(Equal("test-system"))
+		Expect(providerConfig.UserData).To(Equal(base64.StdEncoding.EncodeToString([]byte("test-userData"))))
+		Expect(providerConfig.Cores).To(Equal(parseFloat(expectedCores)))
+		Expect(providerConfig.Memory).To(Equal(parseFloat(expectedMemory)))
+		Expect(providerConfig.Disk).To(Equal(parseFloat(expectedDisk)))
+		Expect(providerConfig.SystemNamespace).To(Equal(systemNamespace))
+	},
 
-	DescribeTable("IBM PowerPC Connection Test",
-		func(instanceId string, expectedError string) {
-			service, err := config.authenticatedService(context.Background(), client)
-			Expect(err).NotTo(HaveOccurred())
-			_, err = service.GetInstanceAddress(context.Background(), instanceId)
-			if expectedError != "" {
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(ContainSubstring(expectedError))
-			} else {
-				Expect(err).NotTo(HaveOccurred())
-			}
-		},
-		Entry("Positive test", "test-instance-id", ""),
-		Entry("Negative test - instance not found", "non-existent-instance-id", "not found"),
-		Entry("Negative test - invalid instance id", "invalid-instance-id!", "not found"),
-		Entry("Negative test - instance id with special characters", "test-instance-id!@#$%^&amp;*()", "not found"),
-		Entry("Negative test - empty instance ID", "", "instance ID cannot be empty"),
-		Entry("Negative test - nil service", nil, "service is nil"),
-		Entry("Negative test - nil instance ID", nil, "instance ID is nil"),
-	)
-})
+	Entry("Positive - valid config map keys", "power-rhtap-prod-2", map[string]string{
+		"memory": "10.0",
+		"cores":  "2.0",
+		"disk":   "100"}, "2.0", "10.0", "100"),
+	Entry("Negative - nonexistant platform name", "koko-hazamar", map[string]string{
+		"memory": "10.0",
+		"cores":  "2.0",
+		"disk":   "300"}, "2.0", "10.0", "300"),
+	Entry("Negative - missing config data", "ppc6", map[string]string{
+		"memory": "",
+		"cores":  "",
+		"disk":   ""}, "0.25", "2", "100"),
+	Entry("Negative - non-numeral config data", "ppc6", map[string]string{
+		"memory": "koko-hazamar",
+		"cores":  "koko-hazamar",
+		"disk":   "koko-hazamar"}, "0.25", "2", "100"),
+)
