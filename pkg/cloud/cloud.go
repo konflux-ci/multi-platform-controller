b757f7649e801a9b695da4c123176d25fe531f1b
Emily Keefe <ekeefe@redhat.com>
2025-02-25 14:02:19 -0500

    IBM Power Systems Cloud Provider code cleanup (part 3)
    * Variables were renamed for clarity in the helper file
    * More error messages in the helper file were prefaced with detail
    * The function for getting the PCloud ID was edited to handle incorrectly
      formatted ID use cases
    * Helper file function names were changed to closely match IBM System Z's

--- pkg/cloud/cloud.go
+++ pkg/cloud/cloud.go
@@ -2,8 +2,9 @@ package cloud
 
 import (
 	"context"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const InstanceTag = "multi-platform-instance"
@@ -11,7 +12,7 @@ const InstanceTag = "multi-platform-instance"
 type CloudProvider interface {
 	LaunchInstance(kubeClient client.Client, ctx context.Context, name string, instanceTag string, additionalInstanceTags map[string]string) (InstanceIdentifier, error)
 	TerminateInstance(kubeClient client.Client, ctx context.Context, instance InstanceIdentifier) error
-	// GetInstanceAddress this only returns an error if it is a permanant error and the host will not ever be available
+	// GetInstanceAddress this only returns an error if it is a permanent error and the host will not ever be available
 	GetInstanceAddress(kubeClient client.Client, ctx context.Context, instanceId InstanceIdentifier) (string, error)
 	CountInstances(kubeClient client.Client, ctx context.Context, instanceTag string) (int, error)
 	ListInstances(kubeClient client.Client, ctx context.Context, instanceTag string) ([]CloudVMInstance, error)
