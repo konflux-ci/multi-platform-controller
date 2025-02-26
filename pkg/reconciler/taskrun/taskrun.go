e56abed80a8108a98448086fb2d8eb8ac6e0b2bd
Emily Keefe <ekeefe@redhat.com>
2025-02-26 10:46:23 -0500

    AWS EC2 Cloud Provider code cleanup (part 2) (#419)
    * Variables were renamed for clarity
    * More error messsages were prefaced with detail for clarity
    * Non-CloudProvider interface functions were moved to a helper file

--- pkg/reconciler/taskrun/taskrun.go
+++ pkg/reconciler/taskrun/taskrun.go
@@ -112,7 +112,7 @@ func newReconciler(mgr ctrl.Manager, operatorNamespace string) reconcile.Reconci
 		eventRecorder:     mgr.GetEventRecorderFor("MultiPlatformTaskRun"),
 		operatorNamespace: operatorNamespace,
 		platformConfig:    map[string]PlatformConfig{},
-		cloudProviders:    map[string]func(platform string, config map[string]string, systemNamespace string) cloud.CloudProvider{"aws": aws.Ec2Provider, "ibmz": ibm.CreateIbmZCloudConfig, "ibmp": ibm.CreateIbmPowerCloudConfig},
+		cloudProviders:    map[string]func(platform string, config map[string]string, systemNamespace string) cloud.CloudProvider{"aws": aws.CreateEc2CloudConfig, "ibmz": ibm.CreateIbmZCloudConfig, "ibmp": ibm.CreateIbmPowerCloudConfig},
 	}
 }
 
