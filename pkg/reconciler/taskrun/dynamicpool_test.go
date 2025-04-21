2a1674197ded033514aa80b1bba11b041a4c1890
meyrevived <84704676+meyrevived@users.noreply.github.com>
2025-04-22 02:42:21 +0300

    Update pkg/reconciler/taskrun/dynamicpool_test.go
    Co-authored-by: Andy Sadler <ansadler@redhat.com>

--- pkg/reconciler/taskrun/dynamicpool_test.go
+++ pkg/reconciler/taskrun/dynamicpool_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 
-	error_client "github.com/konflux-ci/multi-platform-controller/tests/testing_utils"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
