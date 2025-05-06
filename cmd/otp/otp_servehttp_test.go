f5dbde76dd66e333450b839da4b54672d2eddf16
meyrevived <84704676+meyrevived@users.noreply.github.com>
2025-05-06 14:29:21 +0300

    Apply suggestions from code review
    Co-authored-by: Francesco Ilario <filario@redhat.com>

--- cmd/otp/otp_servehttp_test.go
+++ cmd/otp/otp_servehttp_test.go
@@ -53,7 +53,7 @@ var _ = Describe("ServeHTTP handlers", Serial, func() {
 	Describe("Testing otp.ServeHTTP", func() {
 		var testOtp *otp
 
-		JustBeforeEach(func() {
+		BeforeEach(func() {
 			testOtp = NewOtp(&logCapture.Logger)
 		})
 
@@ -94,7 +94,7 @@ var _ = Describe("ServeHTTP handlers", Serial, func() {
 	Describe("Testing storekey.ServeHTTP", func() {
 		var testStorekey *storekey
 
-		JustBeforeEach(func() {
+		BeforeEach(func() {
 			testStorekey = NewStoreKey(&logCapture.Logger)
 		})
 
