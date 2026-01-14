0c598c990be0bc24abfae65465b3e59263b89914
Andy Sadler <ansadler@redhat.com>
2026-01-14 16:58:05 -0600

    util: add retrying client
    Implement a retrying client, which will automatically attempt to retry a
    request if the error returned is a 500-level server error.  This will
    help in cases where apiserver or etcd is running into spurious issues,
    and a simple retry of the request would cause it to succeed.
    
    v2: address comments from @meyrevived
    
    Fixes: KONFLUX-10710
    Signed-off-by: Andy Sadler <ansadler@redhat.com>

---
+++ pkg/util/subresource_client.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2026 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type retrySubresourceClient struct {
+	inner   client.SubResourceClient
+	backoff wait.Backoff
+}
+
+// Create implements client.SubResourceClient.
+func (r *retrySubresourceClient) Create(ctx context.Context, obj client.Object, subResource client.Object, opts ...client.SubResourceCreateOption) error {
+	return retry.OnError(r.backoff, errors.IsInternalError, func() error {
+		return r.inner.Create(ctx, obj, subResource, opts...)
+	})
+}
+
+// Get implements client.SubResourceClient.
+func (r *retrySubresourceClient) Get(ctx context.Context, obj client.Object, subResource client.Object, opts ...client.SubResourceGetOption) error {
+	return retry.OnError(r.backoff, errors.IsInternalError, func() error {
+		return r.inner.Get(ctx, obj, subResource, opts...)
+	})
+}
+
+// Patch implements client.SubResourceClient.
+func (r *retrySubresourceClient) Patch(ctx context.Context, obj client.Object, patch client.Patch, opts ...client.SubResourcePatchOption) error {
+	return retry.OnError(r.backoff, errors.IsInternalError, func() error {
+		return r.inner.Patch(ctx, obj, patch, opts...)
+	})
+}
+
+// Update implements client.SubResourceClient.
+func (r *retrySubresourceClient) Update(ctx context.Context, obj client.Object, opts ...client.SubResourceUpdateOption) error {
+	return retry.OnError(r.backoff, errors.IsInternalError, func() error {
+		return r.inner.Update(ctx, obj, opts...)
+	})
+}
+
+var _ client.SubResourceClient = &retrySubresourceClient{}
