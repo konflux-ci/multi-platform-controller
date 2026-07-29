b398b21fbe72fcd2db20bb1c50b62428f0745ab7
Meirav Rath <84704676+meyrevived@users.noreply.github.com>
2026-07-29 12:16:51 +0300

    Update pkg/ibm/ibmz_helpers_test.go
    Co-authored-by: Francesco Ilario <filario@redhat.com>

--- pkg/ibm/ibmz_helpers_test.go
+++ pkg/ibm/ibmz_helpers_test.go
@@ -21,8 +21,8 @@ var _ = Describe("IBM s390x Helper Functions", func() {
 
 	Describe("The checkIfIpIsLive function", func() {
 		When("provided with an unresolvable address", func() {
-			It("should return an error instead of panicking", func() {
-				err := checkIfIpIsLive(context.TODO(), "invalid-host-that-does-not-resolve.example.invalid")
+			It("should return an error instead of panicking", func(ctx context.Context) {
+				err := checkIfIpIsLive(ctx, "invalid-host-that-does-not-resolve.example.invalid")
 				Expect(err).Should(HaveOccurred())
 			})
 		})
